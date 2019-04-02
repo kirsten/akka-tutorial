7c647c57ef242b3b49f67f7d4cf6b8590a934314
Kirsten Nordine <kirstennordine@gmail.com>
2019-04-01 23:39:37 -0400

    Add a few notes to AkkaQuickstart.scala for reference

--- src/main/scala/com/example/AkkaQuickstart.scala
+++ src/main/scala/com/example/AkkaQuickstart.scala
@@ -4,6 +4,7 @@ import akka.actor.{ Actor, ActorLogging, ActorRef, ActorSystem, Props }
 
 object Greeter {
 
+  // Props is a configuration class to specify options for the creation of actors
   def props(message: String, printerActor: ActorRef): Props = Props(new Greeter(message, printerActor))
 
   final case class WhoToGreet(who: String)
@@ -15,6 +16,8 @@ class Greeter(message: String, printerActor: ActorRef) extends Actor {
   import Greeter._
   import Printer._
 
+  // An Actor can have state. Accessing or mutating the internal state of an
+  // Actor is fully thread safe since it is protected by the Actor model.
   var greeting = ""
 
   def receive = {
@@ -44,8 +47,11 @@ class Printer extends Actor with ActorLogging {
 object AkkaQuickstart extends App {
   import Greeter._
 
+  // The ActorSystem acts as a container for Actors and manages their life-cycles
   val system: ActorSystem = ActorSystem("helloAkka")
 
+  // `actorOf` is a factory method creates Actors and takes two parameters,
+  // a configuration object called Props and a name.
   val printer: ActorRef = system.actorOf(Printer.props, "printerActor")
 
   val howdyGreeter: ActorRef =
