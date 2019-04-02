4ccd8d1b9b212b96c39f1cdf5de25e07a288271a
Kirsten Nordine <kirstennordine@gmail.com>
2019-04-01 22:34:14 -0400

    Remove all the comments

--- src/main/scala/com/example/AkkaQuickstart.scala
+++ src/main/scala/com/example/AkkaQuickstart.scala
@@ -1,21 +1,16 @@
-//#full-example
 package com.example
 
 import akka.actor.{ Actor, ActorLogging, ActorRef, ActorSystem, Props }
 
-//#greeter-companion
-//#greeter-messages
 object Greeter {
-  //#greeter-messages
+
   def props(message: String, printerActor: ActorRef): Props = Props(new Greeter(message, printerActor))
-  //#greeter-messages
+
   final case class WhoToGreet(who: String)
+
   case object Greet
 }
-//#greeter-messages
-//#greeter-companion
 
-//#greeter-actor
 class Greeter(message: String, printerActor: ActorRef) extends Actor {
   import Greeter._
   import Printer._
@@ -26,25 +21,17 @@ class Greeter(message: String, printerActor: ActorRef) extends Actor {
     case WhoToGreet(who) =>
       greeting = message + ", " + who
     case Greet           =>
-      //#greeter-send-message
       printerActor ! Greeting(greeting)
-      //#greeter-send-message
   }
 }
-//#greeter-actor
 
-//#printer-companion
-//#printer-messages
 object Printer {
-  //#printer-messages
+
   def props: Props = Props[Printer]
-  //#printer-messages
+
   final case class Greeting(greeting: String)
 }
-//#printer-messages
-//#printer-companion
 
-//#printer-actor
 class Printer extends Actor with ActorLogging {
   import Printer._
 
@@ -53,29 +40,21 @@ class Printer extends Actor with ActorLogging {
       log.info("Greeting received (from " + sender() + "): " + greeting)
   }
 }
-//#printer-actor
 
-//#main-class
 object AkkaQuickstart extends App {
   import Greeter._
 
-  // Create the 'helloAkka' actor system
   val system: ActorSystem = ActorSystem("helloAkka")
 
-  //#create-actors
-  // Create the printer actor
   val printer: ActorRef = system.actorOf(Printer.props, "printerActor")
 
-  // Create the 'greeter' actors
   val howdyGreeter: ActorRef =
     system.actorOf(Greeter.props("Howdy", printer), "howdyGreeter")
   val helloGreeter: ActorRef =
     system.actorOf(Greeter.props("Hello", printer), "helloGreeter")
   val goodDayGreeter: ActorRef =
     system.actorOf(Greeter.props("Good day", printer), "goodDayGreeter")
-  //#create-actors
 
-  //#main-send-messages
   howdyGreeter ! WhoToGreet("Akka")
   howdyGreeter ! Greet
 
@@ -87,7 +66,4 @@ object AkkaQuickstart extends App {
 
   goodDayGreeter ! WhoToGreet("Play")
   goodDayGreeter ! Greet
-  //#main-send-messages
 }
-//#main-class
-//#full-example
